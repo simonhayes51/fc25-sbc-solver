b39a0fe2a045993f33f7dde79dc9aeb2679d2a0e
simonhayes51 <simonhayes_@hotmail.com>
2025-08-31 22:11:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-# main.py
 from fastapi import FastAPI
 from app.routes import sbc2_db_render as sbc2
 
 app = FastAPI(title="FUTGG SBC Scraper")
 
-# Mount the SBC router
-app.include_router(sbc2.router)
-
 @app.get("/")
 async def root():
     return {"status": "ok", "message": "SBC scraper API running"}
+
+# Mount routes
+app.include_router(sbc2.router)
